0cd6a4a49f90ac048e2c224924ce2b1adfa0e9de
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-06-21 15:38:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  let table = {
+      "2": "abc",
+      "3": "def",
+      "4": "ghi",
+      "5": "jkl",
+      "6": "mno",
+      "7": "pqrs",
+      "8": "tvu",
+      "9": "wxyz"
+  }
+  let ans =[];
+  
+  function solve (currentIndex, calculatedString){
+      if(input_digit.length <= currentIndex){
+          ans.push(calculatedString);
+          return;
+      }
+      let currentchar = input_digit[currentIndex];
+      for(let i =0; i < table[currentchar].length ; i++){
+          solve( currentaIndex+1 , calculatedString + table[currentchar])
+      }
+  }
+  solve(0,"")
+  return ans;
 }
 
 module.exports = letterCombinations;
